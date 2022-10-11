f357b0e4fc6a439376f71a50b9312d54f80a755e
NicollyAlves <nicollyalvesdasilva12@gmail.com>
2022-10-11 11:54:46 -0400

    atividae tema-dark-light

---
+++ tema-dark-light/src/components/app.style.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const AppContainer = styled.div`
+    height: 37vh;
+    display: flex;
+    justify-content: center;
+    padding: 300px;
+
+    background-color: ${(props) => props.theme.backgroundColor};
+    color: ${(props) => props.theme.color};
+    transition: background-color 0.8s linear;
+`
+export const MainSection = styled.div`
+    text-align: center;
+`
